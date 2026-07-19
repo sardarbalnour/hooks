637a9e63536d1d731ee8e251028eec324b2f0e2a
Sardar balnour <balnour.sardar@gmail.com>
2026-07-19 18:28:08 +0330

    useRef & DOM

--- src/components/RefComponent.jsx
+++ src/components/RefComponent.jsx
@@ -1,18 +1,23 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 function RefComponent() {
-  const num = useRef(0);
+  const input = useRef(null);
+
+  useEffect(() => {
+    input.current.focus();
+  }, []);
 
   console.log("render");
 
   const clickHandler = () => {
     num.current++;
-    console.log(num)
+    console.log(num);
   };
 
   return (
     <div>
       RefComponent
+      <input type="text" ref={input} />
       <button onClick={clickHandler}>+</button>
     </div>
   );
