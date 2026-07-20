94cccf1a077066b1929fe598f892c1d3a2a5266a
Sardar balnour <balnour.sardar@gmail.com>
2026-07-20 16:32:55 +0330

    useMemo

--- src/components/Memocomponent.jsx
+++ src/components/Memocomponent.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import ChildComponent from "./ChildComponent";
 
 function Memocompoonent() {
   const [count, setCount] = useState(0);
   const [num, setNum] = useState(0);
 
-  console.log("memo is rendered");
+  const slow = useMemo(() => {
+    for (let index = 0; index < 1000000000; index++) {}
+    return count;
+  }, [count]);
+
   return (
     <div>
-      <span>{count}</span>
+      <span>{slow}</span>
       <button onClick={() => setCount((count) => count + 1)}>+</button>
-      <ChildComponent num={num} />
+      {/* <ChildComponent num={num} /> */}
+      <br />
       <span>{num}</span>
       <button onClick={() => setNum((num) => num + 1)}>+</button>
     </div>
