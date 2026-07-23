40b2a85f2a759fa7efed8709926dc311e26fe799
Sardar balnour <balnour.sardar@gmail.com>
2026-07-23 11:40:44 +0330

    Concurrency

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Memocompoonent from "./components/Memocomponent";
 import RefComponent from "./components/RefComponent";
+import TransitionComponent from "./components/TransitionComponent";
 
 function App() {
   return (
     <>
       hello world
       {/* <RefComponent /> */}
-      <Memocompoonent />
+      {/* <Memocompoonent /> */}
+      <TransitionComponent />
     </>
   );
 }
---
+++ src/components/TransitionComponent.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function arrayCreator(number = 10) {
+  const myArray = [];
+  for (let index = number; index > 0; index--) {
+    myArray.push(index);
+  }
+
+  return myArray;
+}
+
+function TransitionComponent() {
+  console.log("transition component is rendered");
+  const [value, setValue] = useState("");
+  const [counter, setCounter] = useState(arrayCreator());
+
+  const changeHandler = (event) => {
+    setValue(event.target.value);
+    setCounter(arrayCreator(event.target.value));
+  };
+
+  return (
+    <div>
+      <input type="text" value={value} onChange={changeHandler} />
+      {counter.map((i) => (
+        <p key={i}>{i}</p>
+      ))}
+    </div>
+  );
+}
+
+export default TransitionComponent;
